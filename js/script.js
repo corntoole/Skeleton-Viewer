9c3ecdf9c12db6c26c35853187156aba256883bb
Cornelius Toole <cornelius.toole@gmail.com>
2012-02-10 12:02:45 -0600

    I cleaned up the code from the examples on which this app is based. Right now this app justs processes the text from your links and displays a list of the number of obcurrences of each word. The next phase is to write code to visualize this data, likely as a tag cloud.

--- js/script.js
+++ js/script.js
@@ -15,60 +15,45 @@ function sortDict(d) {
 }
 
 function loadLinks(){
-    $.getJSON(baseUrl + '/Me/links/', {'limit':100, 'offset': offset, 'fields':'{"title":1,"embed":1}'}, function(data) {
+    $.getJSON(baseUrl + '/Me/links/', {'limit':20, 'offset': offset, 'fields':'{"title":1,"text":1,"embed":1}', 'full':true}, function(data) {
         if(!data || !data.length) {
-            alert("no data!");
+            console.log("no data!");
             return;
         }
-        var tokens;
+        console.log(data);
+        //var tokens = [];
         var tokens_dict = {};
         var html = '';
         for(var i in data)
         {
-            //if(!data[i].embed || data[i].embed.type != "photo") continue;
             if(!data[i].text ) continue;
             var text = data[i].text;
-            var title = data[i].title;
-            tokens.concat(text.match(/\w/g));
-            tokens.concat(title.match(/\w/g));
+            //var title = data[i].title;
+            text += ' '+data[i].title;
+            //TODO: Fix this hack where I concat text and title b/c I might want to analyze them separately
+            var tokens = text.match(/(\w)+/g);
+            //tokens.concat(title.match(/(\w)+/g));
+            //console.log(tokens);
             for (var t in tokens) {
-                if (!tokens_dict[t]) tokens_dict[t] = 0;
-                tokens_dict[t]+=1;
-            }   
-            // var p = data[i];
-//             html += '<li><a id="url-'+p._id+'" href="#" title="" target="_blank"><img src="'+p.embed.url+'" /><span id="title-'+p._id+'"></span></a></li>';
-//             $(function() {
-//                 var id = p._id;
-//                 $.getJSON(baseUrl + '/Me/links/encounters/'+p._id, function(encounters) {
-//                     if(!encounters || !encounters.length) return;
-//                     $("#title-"+id).text(encounters[0].from);
-//                     $("#url-"+id).attr("href",encounters[0].link);
-//                 });
-//             });
-//             console.log(data[i]);
-        }
+               var tok = tokens[t];
+                if (!tokens_dict[tok]) tokens_dict[tok] = 0;
+                tokens_dict[tok]+=1;
+            } 
+            console.log(tokens_dict);
+               }
         var tokens_dict_sorted = sortDict(tokens_dict);
         for ( var entry in tokens_dict_sorted) {
             html += '<li>#'+entry+'  '+tokens_dict_sorted[entry]+'</li>\n';
         }
         console.log(html);
-        html += $("#test").html();
+        html = $("#test").html() + html;
         $("#test").html(html);
     });
 }
 
 $(function() {
-    // be careful with the limit, some people have large datasets ;)
-    $.getJSON(baseUrl + '/Me/photos/', {'limit':10}, function(data) {
-        console.log(data);
-        if(!data || !data.length) return;
-        var html = "";
-        for(var i in data)
-        {
-            html += "<img src='"+data[i].url+"' width='200' /> ";
-        }
-        $("#test").html(html);
-        loadLinks();
-    });
+	var html = "";
+	$("#test").html(html);
+	loadLinks();
 });
 
